ed224e14ffd9a78aa598fc3aefc3e4feabce8c59
Gekayos <127081727+Gekayos@users.noreply.github.com>
2023-03-21 02:21:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+const gameContainer = document.getElementById("game");
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+// TODO: Implement this function!
+let last = null;
+let deb = false;
+function handleCardClick(event) {
+  if(deb) return;
+  deb = true;
+
+  let current = event.target;
+  let currentName = current.className;
+
+  if(currentName == ""){
+    deb = false;
+    return;
+  }
+
+  current.style.backgroundColor = currentName;
+
+  if(last == null){
+    last = current;
+    deb = false;
+  } else if(last != current && last.className == currentName){
+    current.className = "";
+    last.className = "";
+
+    last = null;
+    deb = false;
+  } else {
+    delay(1000).then(() => {
+      current.style.backgroundColor = "white";
+      last.style.backgroundColor = "white";
+      last = null;
+      deb = false;
+    })
+  }
+  // you can use event.target to see which element was clicked
+  console.log("you just clicked", event.target);
+}
+ 
+
+
+
+
+
+
+
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
